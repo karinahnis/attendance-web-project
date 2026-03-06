293f9a84399f01d51cd9469f813bbd38fa6b7f14
Wandi <karinahnis48@gmail.com>
2026-03-06 21:02:23 +0700

    menghapus typo

--- database/migrations/2026_03_06_051203_create_courses_assignments_table.php
+++ database/migrations/2026_03_06_051203_create_courses_assignments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('courses_assignments_', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelet0-z5988kk e('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('course_code');
             $table->foreign('course_code')->references('code')->on('courses')->onDelete('cascade');
             $table->timestamps();
