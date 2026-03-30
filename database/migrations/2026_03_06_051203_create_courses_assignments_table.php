99d90c235c694ec3f67de79c63a3050aeb2b9a68
irpanNoob <irpannwawi@gmail.com>
2026-03-30 17:41:19 +0700

    fix: Correct table name in migration for courses_assignments

--- database/migrations/2026_03_06_051203_create_courses_assignments_table.php
+++ database/migrations/2026_03_06_051203_create_courses_assignments_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('courses_assignments_');
+        Schema::dropIfExists('courses_assignments');
     }
 };
